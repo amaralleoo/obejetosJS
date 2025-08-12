f3588340578fcc2eddd17279120fa73e6379bffa
amaralleoo <leonardoamaralalves1@gmail.com>
2025-08-12 19:56:09 -0300

    EXE1

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ for(let i = 0; i < 3; i++){
     let marca = prompt(`Informe a marca do veiculo ${i+1}: `)
     let modelo = prompt(`Informe o modelo do veiculo ${i+1}: `)
     let ano = Number(prompt(`Informe o ano do veiculo ${i+1}: `))
-    let cv = Number(prompt(`Informe o CV do veiculo ${i+1}: `))
+    let cv = Number(prompt(`Informe o CV's do veiculo ${i+1}: `))
     let torque = Number(prompt(`Informe o torque do veiculo ${i+1}: `))
 
     let car1 = {
@@ -66,3 +66,36 @@ for(let i = 0; i < 3; i++){
 }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let biblioteca = []
+
+for(let i = 0; i < 5; i++){
+    let titulo = prompt(`Informe o título do livro ${i+1}: `)
+    let autor = prompt(`Informe o autor do livro ${i+1}: `)
+    let anoPubli = Number(prompt(`Informe o ano de publicação do livro ${i+1}: `))
+    let qntPag = Number(prompt(`Informe a quantidade de paginas do livro ${i+1}: `))
+
+    let livro = {
+        titulo: titulo,
+        autor: autor,
+        anoPubli: anoPubli,
+        qntPag: qntPag
+    };
+
+    biblioteca.push(livro);
+}
+
+let pag300
+let maior = biblioteca[0]
+let soma = 0
+for(let i = 0; i < biblioteca.length; i++){
+    if(biblioteca[i].pag300 > 300){
+        console.log(biblioteca[i])
+    }
+    if(biblioteca[i].pag300 > maior.pag300){
+        maior = biblioteca[i]
+    }
+    soma = soma + biblioteca[i].pag300
+}
+console.log(`O titulo do livro com mais páginas é ${maior.titulo}`)
+console.log(`A média de páginas dos livros é ${soma / 5}`)
